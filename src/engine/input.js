6f6a6087e4a49f254f0e7c6c6ca099454dac1fbc
oweniscool-ai <owenglanzman@icloud.com>
2026-09-17 13:23:11 -0700

    Create input.js

---
+++ src/engine/input.js
@@ -0,0 +1,7 @@
+export class Input {
+  constructor() {
+    this.keys = {};
+    window.addEventListener('keydown', e => this.keys[e.code] = true);
+    window.addEventListener('keyup', e => this.keys[e.code] = false);
+  }
+}
