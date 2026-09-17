995a75bbc0b698a43b1ee229aff70eac03ddf84d
oweniscool-ai <owenglanzman@icloud.com>
2026-09-17 16:37:25 -0700

    Create sfx_registry.js

---
+++ src/audio/sfx_registry.js
@@ -0,0 +1,10 @@
+export const SFX_REGISTRY = {
+  HIT_LIGHT: 'assets/sfx/hit_light.wav',
+  HIT_HEAVY: 'assets/sfx/hit_heavy.wav',
+  SHIELD_BLOCK: 'assets/sfx/shield_block.wav',
+  JUMP: 'assets/sfx/jump.wav',
+  DASH: 'assets/sfx/dash.wav',
+  BLASTZONE_KO: 'assets/sfx/blastzone_ko.wav',
+  BUTTON_HOVER: 'assets/sfx/ui_hover.wav',
+  BUTTON_CLICK: 'assets/sfx/ui_click.wav'
+};
