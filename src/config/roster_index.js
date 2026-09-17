db477e04be027a4288b21839f09a276ecfd91208
oweniscool-ai <owenglanzman@icloud.com>
2026-09-17 13:20:10 -0700

    Create roster_index.js

---
+++ src/config/roster_index.js
@@ -0,0 +1,5 @@
+export const ROSTER_INDEX = [
+  { id: "kathy", name: "Kathy the Karen", file: "src/characters/file_0016_kathy.js" },
+  { id: "andy", name: "Andy the Airfryer", file: "src/characters/file_0017_andy.js" },
+  { id: "keven", name: "Keyboard Warrior Keven", file: "src/characters/file_0018_keven.js" }
+];
