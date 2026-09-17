617a3619a0c3d08e49dd78e77d560cc2b1b62863
oweniscool-ai <owenglanzman@icloud.com>
2026-09-17 04:02:40 -0700

    Delete js directory

--- js/kathy.js
+++
@@ -1,1 +0,0 @@
-window.KathyCharacter = { name: "Kathy the Karen", archetype: "Combo", weight: 80, speed: 6.8, jumpPower: 14, damage: 9, knockback: 7, hasShield: false, hasSDI: false };
--- js/owentogud.js
+++
@@ -1,1 +0,0 @@
-window.OwentogudCharacter = { name: "Owentogud", archetype: "Heavy", weight: 150, speed: 5.0, jumpPower: 13, damage: 22, knockback: 18, isRestricted: true, hasShield: false, hasSDI: false };
