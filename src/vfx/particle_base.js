1de06a67ae3d2926ac869ac75529380051227a1e
oweniscool-ai <owenglanzman@icloud.com>
2026-09-18 12:57:59 -0700

    Create particle_base.js

---
+++ src/vfx/particle_base.js
@@ -0,0 +1,34 @@
+export class Particle {
+  constructor(x, y, vx, vy, life, color, size) {
+    this.x = x;
+    this.y = y;
+    this.vx = vx;
+    this.vy = vy;
+    this.life = life;
+    this.maxLife = life;
+    this.color = color;
+    this.size = size;
+    this.alpha = 1.0;
+  }
+
+  update(delta = 1) {
+    this.x += this.vx * delta;
+    this.y += this.vy * delta;
+    this.life -= delta;
+    this.alpha = Math.max(0, this.life / this.maxLife);
+  }
+
+  render(ctx) {
+    ctx.save();
+    ctx.globalAlpha = this.alpha;
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.restore();
+  }
+
+  isDead() {
+    return this.life <= 0;
+  }
+}
